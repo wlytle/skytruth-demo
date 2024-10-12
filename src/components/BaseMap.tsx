e5ca4f3c1832737033d591ebb03bbd630a9b5ac5
Will Lytle <welytle@arizona.edu>
2024-10-12 16:53:28 -0700

    Adds MVP of earthquake pop over on click

--- src/components/BaseMap.tsx
+++ src/components/BaseMap.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from "react";s
+import { useEffect, useRef, useState } from "react";
 import mapboxgl, { GeoJSON, Map } from "mapbox-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./BaseMap.css";
 
 function BaseMap() {
   const mapRef = useRef<Map | undefined>();
@@ -14,6 +15,7 @@ function BaseMap() {
   const earthquakeUrl =
     "https://earthquake.usgs.gov/fdsnws/event/1/query?basicmagnitude=&minmagnitude=2.5&maxmagnitude=10&starttime=2022-01-01&endtime=2022-12-31&maxlatitude=40&minlongitude=-200&maxlongitude=0&minlatitude=30&format=geojson";
   
+    const outdoorsUrl = "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=fbb8fa64d2014e5da62dd21c5d665dd1";
 
   useEffect(() => {
     const setReady = () => setIsMapReady(true)    
@@ -22,8 +24,8 @@ function BaseMap() {
       mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY;
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
-        center: [-114, 30],
-        zoom: 6,
+        center: [-101, 38],
+        zoom: 4,
       });
       // Listen for map laod to complete 
       mapRef.current.on("load",  setReady)
@@ -42,8 +44,8 @@ function BaseMap() {
       return;
     }
 
-    if (showEarthquakes) {
-      mapRef.current?.addLayer({
+    if (showEarthquakes && mapRef.current) {
+      mapRef.current.addLayer({
       id: "earthquakes-layer",
       type: "circle",
       source: "earthquakes",
@@ -53,6 +55,33 @@ function BaseMap() {
         "circle-color": "red",
         "circle-stroke-color": "white",
       },
+    });
+
+    mapRef.current.on('click', 'earthquakes-layer', (e) => {
+      // Access the properties of the clicked marker
+      const feature = e?.features[0];
+      const magnitude = feature?.properties?.mag
+      
+
+      const makePopup = (feature) => {
+        const date = new Date(feature?.properties.time)
+        const magnitude = feature?.properties?.mag;
+        const place = feature?.properties?.place;
+        console.log("event", feature, place, magnitude, date);
+        return (
+          `<div class=eq-popup>
+          <h3 class=pop-text>${place}</h3>
+          <span class=pop-text>Magnitude: ${magnitude}</span>
+          <span class=pop-text>Time: ${date}</span>
+          </div>`
+        )
+      } 
+    
+      // Create a popup and display it
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(makePopup(feature))
+        .addTo(mapRef.current);
     });
   } else {
     mapRef.current?.removeLayer("earthquakes-layer")
