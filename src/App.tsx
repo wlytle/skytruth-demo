0f0901002a835048f59a58b02d797cc74de308f9
Will Lytle <welytle@arizona.edu>
2024-10-13 14:22:59 -0700

    add functionality for outdoor raster tile layer

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import "./App.css";
 function App() {
   return (
     <>
-    <ErrorBoundary fallback={<p>⚠️Something went wrong</p>}>
-      <BaseMap />
-    </ErrorBoundary>
+      <ErrorBoundary fallback={<p>⚠️Something went wrong</p>}>
+        <BaseMap />
+      </ErrorBoundary>
     </>
   );
 }
